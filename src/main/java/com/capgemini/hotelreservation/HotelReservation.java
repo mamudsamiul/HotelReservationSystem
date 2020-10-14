74a6ca8d1d7fa307323948e48f44ad20122448b1
mamudsamiul <samiulmamud1995@gmail.com>
2020-10-14 23:19:14 +0530

    (Samiul Mamud) created: Hotel Reservation system program is created

---
+++ src/main/java/com/capgemini/hotelreservation/HotelReservation.java
@@ -0,0 +1,9 @@
+package com.capgemini.hotelreservation;
+
+public class HotelReservation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Hotel Reservation Program");
+	}
+
+}
